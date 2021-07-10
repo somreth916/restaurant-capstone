b5c6b24671ba7a4d8be9eab7e5c21bc086cd5191
Somreth Peng <somreth916@gmail.com>
2021-07-10 01:49:41 -0700

    updated backend

--- back-end/src/reservations/reservations.router.js
+++ back-end/src/reservations/reservations.router.js
@@ -14,4 +14,15 @@ router
 	.post(controller.create)
 	.all(methodNotAllowed);
 
+router
+	.route("/:reservation_id/status")
+	.put(controller.update)
+	.all(methodNotAllowed);
+  
+router
+	.route("/:reservation_id")
+	.get(controller.read)
+	.put(controller.edit)
+	.all(methodNotAllowed);
+	
 module.exports = router;
--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -9,9 +9,15 @@
  const methodNotAllowed = require("../errors/methodNotAllowed");
  
  router
-     .route("/")
-     .get(controller.list)
-     .post(controller.create)
-     .all(methodNotAllowed);
+    .route("/")
+    .get(controller.list)
+    .post(controller.create)
+    .all(methodNotAllowed);
+
+router
+    .route("/:table_id/seat")
+    .put(controller.update)
+    .delete(controller.destroy)
+    .all(methodNotAllowed);
   
  module.exports = router;
